52ceb785f5dbfd61b844bf55a05e200eceec273c
MariaRadmer <maria98frb@gmail.com>
2025-09-29 13:36:36 +0200

    added area switch

--- Assets/Scripts/AreaSwitcher.cs
+++ Assets/Scripts/AreaSwitcher.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AreaSwitcher : MonoBehaviour
 {
+
+    public string sceneToLoad;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,14 @@ public class AreaSwitcher : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
